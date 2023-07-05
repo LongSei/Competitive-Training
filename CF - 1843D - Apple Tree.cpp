7984041a39b6a1b5186f96af7ed62835f693e063
LongSei <htaevd@gmail.com>
2023-07-06 02:01:51 +0700

    Add files via upload

---
+++ CF - 1843D - Apple Tree.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// DEBUG TEMPLATE //
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template<typename T>
+void __print(deque<T> dq) {cerr << "{"; while (dq.empty() == false) {auto fr = dq.front(); __print(fr); dq.pop_front(); if (dq.empty() == false) {cerr << ",";}} cerr << "}";}
+template<typename T>
+void __print(queue<T> q) {cerr << "{"; while (q.empty() == false) {auto fr = q.front(); __print(fr); q.pop(); if (q.empty() == false) {cerr << ",";}} cerr << "}";}
+template<typename T>
+void __print(stack<T> st) {cerr << "{"; while (st.empty() == false) {auto fr = st.top(); __print(fr); st.pop(); if (st.empty() == false) {cerr << ",";}} cerr << "}";}
+template<typename T>
+void __print(priority_queue<T> q) {cerr << "{"; while (q.empty() == false) {auto fr = q.top(); __print(fr); q.pop(); if (q.empty() == false) {cerr << ",";}} cerr << "}";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// TEMPLATE FOR SOLVING PROBLEM // 
+// #define endl '\n'
+#define int long long
+#define ii pair<int, int>
+#define pi_ii pair<int, ii>
+#define pii_i pair<ii, int>
+#define p_queue priority_queue 
+#define pb push_back
+#define lb lower_bound  // First element NOT LESS than val
+#define ub upper_bound  // First element GREATER than val
+#define all(x) (x).begin(), (x).end()
+#define min(x, y) (((x) < (y))?(x):(y))
+#define max(x, y) (((x) > (y))?(x):(y))
+#define foru(i, st, ed, range) for(int i = st; i <= ed; i = i + range)
+#define ford(i, st, ed, range) for(int i = st; i >= ed; i = i + range)
+#define faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int INF = 1e15 + 5;
+const int MAX = 5e5 + 5;
+const int mod = 1e9 + 7;
+
+// TEMPLATE FOR DATA STRUCTURE AND ALGORITHM //	
+int fast_pow(int n, int k) {
+	int res = 1;
+    while (k) {
+        if (k & 1) {res = (res * n) % mod; k -= 1;}
+        else {n = (n * n) % mod; k >>= 1;}
+    }
+    return res;
+}
+int GCD(int a, int b) {return b ? GCD (b, a % b) : a;}
+int LCM(int a, int b) {return a * b / GCD(a, b);}
+int GetBit(int k, int n) {return (n & (1 << k)) >> k;}
+
+// MAIN CODE //
+vector<int> adjlist[MAX];
+int dp[MAX];
+void dfs(int node, int parent) {
+    if (adjlist[node].size() == 1 && adjlist[node][0] == parent) {
+        dp[node] = 1;
+        return;
+    }
+    for (int v : adjlist[node]) {
+        if (v != parent) {
+            dfs(v, node);
+            dp[node] += dp[v];
+        }
+    }
+}
+void reset(int n) {
+    for (int i = 1; i <= n; i++) {
+        adjlist[i].clear();
+        dp[i] = 0;
+    }
+}
+void solve() {
+    int n; cin >> n;
+    reset(n);
+    for (int i = 1; i <= n - 1; i++) {
+        int u, v; cin >> u >> v;
+        adjlist[u].pb(v);
+        adjlist[v].pb(u);
+    }
+    dfs(1, 1);
+    int q; cin >> q;
+    for (int i= 1; i <= q; i++) {
+        int u, v; cin >> u >> v;
+        cout << dp[u] * dp[v] << endl;
+    }
+}
+
+signed main() {
+	faster;
+	int test = 1;
+	cin >> test; 
+	while (test--) {
+		solve();
+	}
+}
