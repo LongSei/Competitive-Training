c4a2ca4fe445f5992ebe649723ff76f0e02a5138
LongSei <74856548+LongSei@users.noreply.github.com>
2026-03-29 20:50:07 +0700

    Add solution for checking equal strings with operations
    Implement a method to check if two strings can be made equal by comparing characters at even and odd indices.

---
+++ Leetcode - 2840 - Check if Strings Can be Made Equal With Operations II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] even1 = new int[26];
+        int[] odd1 = new int[26];
+        int[] even2 = new int[26];
+        int[] odd2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+
+            if (i % 2 == 0) {
+                even1[c1 - 'a']++;
+                even2[c2 - 'a']++;
+            } else {
+                odd1[c1 - 'a']++;
+                odd2[c2 - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (even1[i] != even2[i]) return false;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (odd1[i] != odd2[i]) return false;
+        }
+
+        return true;
+    }
+}
